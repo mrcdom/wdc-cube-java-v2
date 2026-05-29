ec14503f217555c4959e158d5a9959efe93ded8e
Marcelo Domingos <mrcdom@gmail.com>
2026-05-28 21:01:34 -0300

    ♻️ [REFACTOR] Extraído lógica de criação/projeção de PurchaseInfo e ReceiptForm para os respectivos Services

--- fontes/br.com.wdc.shopping/br.com.wdc.shopping.presentation/src/main/java/br/com/wdc/shopping/presentation/presenter/restricted/home/purchases/PurchasesPanelService.java
+++ fontes/br.com.wdc.shopping/br.com.wdc.shopping.presentation/src/main/java/br/com/wdc/shopping/presentation/presenter/restricted/home/purchases/PurchasesPanelService.java
@@ -1,11 +1,21 @@
 package br.com.wdc.shopping.presentation.presenter.restricted.home.purchases;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
+
+import br.com.wdc.framework.commons.lang.CoerceUtils;
 import br.com.wdc.shopping.domain.criteria.PurchaseCriteria;
 import br.com.wdc.shopping.domain.criteria.PurchaseCriteria.OrderBy;
+import br.com.wdc.shopping.domain.model.Product;
+import br.com.wdc.shopping.domain.model.Purchase;
+import br.com.wdc.shopping.domain.model.PurchaseItem;
 import br.com.wdc.shopping.domain.pagination.Page;
 import br.com.wdc.shopping.domain.repositories.PurchaseRepository;
+import br.com.wdc.shopping.domain.utils.ProjectionValues;
 import br.com.wdc.shopping.presentation.ShoppingApplication;
 import br.com.wdc.shopping.presentation.presenter.restricted.home.structs.PurchaseInfo;
 
@@ -22,8 +32,8 @@ public class PurchasesPanelService {
     }
 
     public List<PurchaseInfo> loadPurchases(PurchaseCriteria criteria) {
-        return repo.fetch(criteria.withProjection(PurchaseInfo.projectionWithItens()))
-                .stream().map(PurchaseInfo::create).toList();
+        return repo.fetch(criteria.withProjection(purchaseProjection()))
+                .stream().map(this::purchaseInfoCreate).toList();
     }
 
     public int countPurchasesOfUser(Long userId) {
@@ -41,11 +51,11 @@ public class PurchasesPanelService {
 
         return repo.fetch(new PurchaseCriteria()
                 .withUserId(userId)
-                .withProjection(PurchaseInfo.projectionWithItens())
+                .withProjection(purchaseProjection())
                 .withOrderBy(OrderBy.DESCENDING),
                 offset != null ? offset : 0,
                 limit != null ? limit : 0)
-                .stream().map(PurchaseInfo::create).toList();
+                .stream().map(this::purchaseInfoCreate).toList();
     }
 
     public Page<PurchaseInfo> fetchPageOfUser(Long userId, int page, int pageSize) {
@@ -55,10 +65,61 @@ public class PurchasesPanelService {
 
         var result = repo.fetchPage(new PurchaseCriteria()
                 .withUserId(userId)
-                .withProjection(PurchaseInfo.projectionWithItens())
+                .withProjection(purchaseProjection())
                 .withOrderBy(OrderBy.DESCENDING), page, pageSize);
 
-        var items = result.items().stream().map(PurchaseInfo::create).toList();
+        var items = result.items().stream().map(this::purchaseInfoCreate).toList();
         return new Page<>(items, result.page(), result.totalPages(), result.totalItems());
+    }
+
+    // :: Internal
+
+    private Purchase purchaseProjection() {
+        var pv = ProjectionValues.INSTANCE;
+
+        var prdPrj = new Product();
+        prdPrj.name = pv.str;
+
+        var itemPrj = new PurchaseItem();
+        itemPrj.price = pv.f64;
+        itemPrj.amount = pv.i32;
+        itemPrj.product = prdPrj;
+
+        var prj = new Purchase();
+        prj.id = pv.i64;
+        prj.buyDate = pv.offsetDateTime;
+        prj.items = Collections.singletonList(itemPrj);
+
+        return prj;
+    }
+
+    private PurchaseInfo purchaseInfoCreate(Purchase src) {
+        if (src == null) {
+            return null;
+        }
+
+        var pv = ProjectionValues.INSTANCE;
+
+        var tgt = new PurchaseInfo();
+        tgt.id = Optional.ofNullable(src.id).orElse(-1L);
+
+        var buyDate = CoerceUtils.asDate(src.buyDate);
+        tgt.date = Optional.ofNullable(buyDate).orElse(pv.date).getTime();
+        tgt.items = new ArrayList<>();
+
+        var total = 0.0;
+        for (var item : Optional.ofNullable(src.items).orElse(Collections.emptyList())) {
+            var price = Optional.ofNullable(item.price).orElse(0.0);
+            var amount = Optional.ofNullable(item.amount).orElse(0);
+            total += price * amount;
+
+            if (item.product != null && StringUtils.isNotBlank(item.product.name)) {
+                tgt.items.add(item.product.name);
+            }
+        }
+
+        tgt.total = total;
+
+        return tgt;
     }
 }
--- fontes/br.com.wdc.shopping/br.com.wdc.shopping.presentation/src/main/java/br/com/wdc/shopping/presentation/presenter/restricted/receipt/structs/ReceiptForm.java
+++ fontes/br.com.wdc.shopping/br.com.wdc.shopping.presentation/src/main/java/br/com/wdc/shopping/presentation/presenter/restricted/receipt/structs/ReceiptForm.java
@@ -1,13 +1,9 @@
 package br.com.wdc.shopping.presentation.presenter.restricted.receipt.structs;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import br.com.wdc.shopping.domain.model.Purchase;
-import br.com.wdc.shopping.domain.utils.ProjectionValues;
-
+@SuppressWarnings("java:S1948")
 public class ReceiptForm implements Serializable {
 
     private static final long serialVersionUID = -3783933804174247490L;
@@ -15,41 +11,5 @@ public class ReceiptForm implements Serializable {
     public Long date;
     public List<ReceiptItem> items;
     public Double total;
-
-    public static Purchase projection() {
-        var pv = ProjectionValues.INSTANCE;
-
-        var prj = new Purchase();
-        prj.buyDate = pv.offsetDateTime;
-        prj.items = Collections.singletonList(ReceiptItem.projection());
-
-        return prj;
-    }
-
-    public static ReceiptForm create(Purchase src) {
-        if (src == null) {
-            return null;
-        }
-
-        var tgt = new ReceiptForm();
-
-        tgt.date = src.buyDate == null ? null : src.buyDate.toInstant().toEpochMilli();
-        tgt.items = new ArrayList<>();
-
-        var total = 0.0;
-        if (src.items != null) {
-            for (var purchaseItem : src.items) {
-                if (purchaseItem.price != null) {
-                    var amount = purchaseItem.amount != null ? purchaseItem.amount : 0;
-                    total += purchaseItem.price * amount;
-                }
-
-                tgt.items.add(ReceiptItem.create(purchaseItem));
-            }
-        }
-        tgt.total = total;
-
-        return tgt;
-    }
 
 }
