185236eb16fd0039f75a1dedb9be1fb7fd04f424
Marcelo Domingos <mrcdom@gmail.com>
2026-06-03 08:31:55 -0300

    📝 [DOCS] context.html para remote.shell.flutter na landing page

--- fontes/br.com.wdc.shopping/br.com.wdc.shopping.view.remote/remote.shell.flutter/flutter.web/build.sh
+++ fontes/br.com.wdc.shopping/br.com.wdc.shopping.view.remote/remote.shell.flutter/flutter.web/build.sh
@@ -7,4 +7,7 @@ DEPLOY_DIR="../../../../../work/frontend/remote.shell.flutter"
 echo "Building Flutter web..."
 flutter build web --base-href="/remote.shell.flutter/" --output="$DEPLOY_DIR" --release
 
+echo "Copying context.html..."
+cp "$(dirname "$0")/context.html" "$DEPLOY_DIR/context.html"
+
 echo "Deploy complete: $DEPLOY_DIR"
